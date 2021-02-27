b9076a78daa0c299040aae8d7856b6b6f135d802
chipfody <60615532+chipfody@users.noreply.github.com>
2021-02-27 18:09:31 -0500

    exam review

--- src/questions/LiveReview.java
+++ src/questions/LiveReview.java
@@ -1,5 +1,6 @@
 package questions;
 
+import java.util.ArrayList;
 import java.util.function.Predicate;
 
 public class LiveReview {
@@ -144,6 +145,126 @@ class University {
             //System.out.println(c.courseName);}
         for (EJavaCourse c : courses) System.out.println(c.courseName);
     }
+}
+
+//ME-Q33
+class ColorPack {
+    static int shadeCount = 12;
+    int getShadeCount() {
+        return shadeCount;
+    }
+}
+class Artist {
+    public static void main(String args[]) {
+        ColorPack pack1 = new ColorPack();
+        System.out.println(pack1.getShadeCount());
+    }
+}
+
+//ME-Q34
+class Laptop {
+    String memory = "1 GB";
+}
+class Workshop {
+    public static void main(String args[]) {
+        Laptop life = new Laptop();
+        repair(life);
+        System.out.println(life.memory);
+    }
+    public static void repair(Laptop laptop) {
+        laptop.memory = "2 GB";
+    }
+}
+
+//ME-Q35
+class Application {
+    public static void main(String... args) {
+        double price = 10;
+        String model;
+
+        if (price > 10)
+            model = "Smartphone";
+        else if (price <= 10)
+            model = "landline";
+        else model = "noline";
+        System.out.println(model);
+    }
+}
+
+//ME-Q38
+class Book {
+    String ISBN;
+    Book(String val) {
+        ISBN = val;
+    }
+}
+class TestEquals {
+    public static void main(String... args) {
+        StringBuilder sb1 = new StringBuilder(5*10);
+        sb1.append("New");
+        sb1.insert(3, 'P');
+        System.out.println(sb1);
+        Book b1 = new Book("1234-4657");
+        Book b2 = new Book("1234-4657");
+        Book b3 = b1;
+        System.out.print(b1.equals(b2) +":" );
+        System.out.print((b1 == b2) + " : ");
+        System.out.print(b3.equals(b1));
+    }
+}
+
+//ME-Q41
+class EJGArrayL {
+    public static void main(String args[]) {
+        ArrayList<String> seasons = new ArrayList<>();
+        System.out.println(seasons.size());
+        seasons.add("Seasons:");
+        seasons.add(1, "Spring");
+        seasons.add(2, "Summer");
+        seasons.add(3, "Autumn");
+        seasons.add(4, "Winter");
+        seasons.remove(2);
+        for (String s : seasons)
+            System.out.print(s + ", ");
+    }
+}
+
+//ME-Q43
+class Whale {
+    public static void main(String args[]) {
+        boolean hungry = false;
+
+        while (hungry=true) { //the condition actually re-assigns hungry to true so the loop will execute infinitely
+            ++Fish.count;
+        }
+        System.out.println(Fish.count);
+    }
+}
+class Fish {
+    static byte count;
+}
+
+//ME-Q44
+class Phones {
+    public static void main(String args[]) {
+        String phones[]= {"BlackBerry", "Android", "iPhone"};
+        for (String phone : phones)
+            System.out.println(phone);
+    }
+}
+
+//ME-Q45
+    class Conversion {
+    Byte b1 = (byte) 100; // 1
+    Integer i1 = (int) 200; // 2
+    Long l1 = (long) 300; // 3
+    //Float f1 = (float) b1 + (int) l1; // 4  can not cast int to a Long
+    //String s1 = 300; // 5  can not assign and int to a String
+    //if(s1 ==(b1 + i1)) // 6 can not compare a String to wrapper class
+    //s1 =(String)500; // 7  can not assign an int to a string by casting -- need to use .valueOf()
+        //else // 8
+    //f1 =(int)100; // 9  can not convert an int to a Float
+       // System.out.println(s1 +":"+f1); // 10
 }
 
 
