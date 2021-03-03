614ab7696158248a7c63844dd7bfa4d6bb1cf14b
chipfody <60615532+chipfody@users.noreply.github.com>
2021-03-03 15:35:45 -0500

    exam review

--- src/oC8Oracle/Moo.java
+++ src/oC8Oracle/Moo.java
@@ -5,6 +5,9 @@ public class Moo  extends Zoo{
     public String coolMethod(int x) {
         return "Moo Baby";
     }
+    boolean givesMilk() {
+        return true;
+    }
     public static void main(String[] args) {
         Zoo moo = new Zoo();
         Zoo moo1 = new Moo();
@@ -12,7 +15,33 @@ public class Moo  extends Zoo{
         System.out.println(("A zoo says, " + moo.coolMethod()));
         System.out.println(("A zoo says, " + moo1.coolMethod()));
         System.out.println(("A zoo says, " + moo2.coolMethod()));
-//        System.out.println(("A zoo says, " + moo1.coolMethod(4)));
         System.out.println(("A zoo says, " + moo2.coolMethod(4)));
+        System.out.println(moo2.givesMilk());
+        System.out.println((((Moo) moo1).givesMilk()));
+//        System.out.println(("A zoo says, " + moo1.coolMethod(4)));
+
     }
 }
+
+interface Gadget {
+    void doStuff();
+
+}
+
+abstract class Electronic {
+    void getPower() {
+        System.out.println("Plug In ");
+    }
+}
+
+class Tablet extends Electronic implements Gadget {
+    public void doStuff() {
+        System.out.println("Show book ");
+    }
+
+    public static void main(String[] args) {
+        new Tablet().getPower();
+        new Tablet().doStuff();
+    }
+}
+
