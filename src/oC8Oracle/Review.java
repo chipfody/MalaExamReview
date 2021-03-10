89a708f0313d75f8bac053c0c9e57c16aeecc0ec
chipfody <60615532+chipfody@users.noreply.github.com>
2021-03-10 16:05:27 -0500

    exam review

--- src/oC8Oracle/Review.java
+++ src/oC8Oracle/Review.java
@@ -16,3 +16,24 @@ class Zoo {
     }
 }
 
+class ReferenceTest{
+    public static void main(String[] args) {
+        int a = 1;
+        ReferenceTest rt = new ReferenceTest();
+        System.out.println("Before modify a = " + a);
+        rt.modifyPrimitive(a);
+        System.out.println("After modify a = " + a);
+        a = rt.modifyPrimitive(a);
+        System.out.println("After re-assigning a = " + a);
+    }
+    int modifyPrimitive(int x) {
+        x = x + 1;
+        System.out.println("Integer in method = " + x);
+        return x;
+    }
+
+    ReferenceTest modifyObject (ReferenceTest rt) {
+        return rt;
+    }
+}
+
