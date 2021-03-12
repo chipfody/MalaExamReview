e7ca4b98b11bb5a8ad2f0874f1c6266ba367f2df
chipfody <60615532+chipfody@users.noreply.github.com>
2021-03-11 21:38:49 -0500

    exam review

--- src/oC8Oracle/Review.java
+++ src/oC8Oracle/Review.java
@@ -37,3 +37,42 @@ class ReferenceTest{
     }
 }
 
+class Foo {
+    static int size = 7;
+
+    static void changeIt(int size) {
+        size += 200;
+        System.out.println("size in changeIt method = " + size);
+    }
+
+    public static void main(String[] args) {
+        Foo foo = new Foo();
+        System.out.println("size = " + size);
+        changeIt(size);
+        System.out.println("size after changeIt = " + size);
+    }
+}
+
+class Telescope {
+    static int magnify = 2;
+
+    public static void main(String[] args) {
+        go();
+    }
+
+    static void go() {
+        magnify = 3;
+        zoomIn();
+    }
+    static void zoomIn() {
+        magnify *= 5;
+        zoomMore(magnify);
+        System.out.println(magnify);
+    }
+
+    static void zoomMore(int magnify) {
+        magnify *= 7;
+    }
+
+}
+
